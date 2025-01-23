106aa842899c23a4df32dc074d011f769d397d23
Anajara <doc.anajara@gmail.com>
2025-01-23 17:59:06 -0300

    at5

---
+++ exercicio2.js
@@ -0,0 +1,27 @@
+// 1. Escreva uma função que receba uma string e retorne o número de caracteres contidos nela.
+// let txt = "Anajara";
+// console.log(txt, "=", txt.length);
+
+function caracter(txt) {
+    return txt.length; 
+}
+
+let txt = "Anajara";
+console.log(txt + " = " + caracter(txt));
+
+
+// 2. Escreva uma função que receba uma string e retorne a mesma string em letras maiúsculas.
+
+// 3. Escreva uma função que receba uma string e retorne a mesma string em letras minúsculas.
+
+// 4. Escreva uma função que receba duas strings e retorne true se elas forem iguais ou false caso contrário.
+
+// 5. Escreva uma função que receba uma string e um número e retorne os primeiros caracteres da string de acordo com o número informado.
+
+// 6. Escreva uma função que recebe uma frase e uma palavra. Retorne a frase com as ocorrências da palavra censuradas/trocadas por ****.
+
+// 7. Escreva uma função que receba uma string e um caractere e retorne o número de ocorrências desse caractere na string.
+
+// 8. Escreva uma função que receba um número e retorne seu dobro. Utilize arrow function.
+
+// 9. Escreva uma função que receba um número e retorne seu quadrado. Utilize arrow function.
